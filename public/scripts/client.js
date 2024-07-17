f6c8c71715d3aebc7343f4c5a2554cc035c7975a
NelsonCGSantos <nelsoncgsantos@gmail.com>
2024-07-17 14:28:41 -0600

    dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,68 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function () {
+  const createTweetElement = function(tweet) {
+    const $tweet = $(`
+        <article class="tweet">
+          <header>
+            <div class="tweet-user">
+              <img src="${tweet.user.avatars}" alt="User avatar">
+              <span>${tweet.user.name}</span>
+            </div>
+            <span class="tweet-handle">${tweet.user.handle}</span>
+          </header>
+          <div class="tweet-content">
+            <p>${tweet.content.text}</p>
+          </div>
+          <footer>
+            <span>${timeago.format(tweet.created_at)}</span>
+            <div class="tweet-icons">
+              <i class="fas fa-flag"></i>
+              <i class="fas fa-retweet"></i>
+              <i class="fas fa-heart"></i>
+            </div>
+          </footer>
+        </article>
+      `);
+    return $tweet;
+  };
+
+  // Render tweets
+  const renderTweets = function(tweets) {
+    $("#tweets-container").empty(); // Clear the tweets container
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $("#tweets-container").append($tweet);
+    }
+  };
+
+  // Testing
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
+    },
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
+
+  // Render Test
+  renderTweets(data);
+});
